3a96e92ef3cdef3b81da9a70cb589b90e3c97e75
lequocgiom <letrung95@gmail.com>
2020-01-16 23:00:30 +0700

    custom exception for unique project identifier error.

--- PPMT/src/main/java/trungle95/personal/example/PPMT/services/ProjectService.java
+++ PPMT/src/main/java/trungle95/personal/example/PPMT/services/ProjectService.java
@@ -3,6 +3,7 @@ package trungle95.personal.example.PPMT.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import trungle95.personal.example.PPMT.domain.Project;
+import trungle95.personal.example.PPMT.exceptions.ProjectIdException;
 import trungle95.personal.example.PPMT.repositories.ProjectRepository;
 
 @Service
@@ -12,9 +13,13 @@ public class ProjectService {
     private ProjectRepository projectRepository;
 
     public Project saveOrUpdateProject(Project project) {
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }
+        catch(Exception e) {
+            throw new ProjectIdException("Project ID '"+project.getProjectIdentifier().toUpperCase() + "' already exist.");
+        }
 
-        //logic
-
-        return projectRepository.save(project);
     }
 }
