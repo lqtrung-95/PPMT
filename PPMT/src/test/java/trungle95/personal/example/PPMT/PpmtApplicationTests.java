87f11807b8aacfba53899cbd94c40cd34d8ebaa4
lequocgiom <letrung95@gmail.com>
2020-01-16 14:58:40 +0700

    first commit

---
+++ PPMT/src/test/java/trungle95/personal/example/PPMT/PpmtApplicationTests.java
@@ -0,0 +1,13 @@
+package trungle95.personal.example.PPMT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
