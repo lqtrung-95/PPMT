08ce34536494f5d111f46a396475160c9a93e921
lequocgiom <letrung95@gmail.com>
2020-01-17 15:40:59 +0700

    added React Router, styling dashboard and project item.

--- ppm-react-frontend/src/App.js
+++ ppm-react-frontend/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Dashboard />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Dashboard exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/addProject" component={AddProject}/>
+      </div>
+    </Router>
   );
 }
 
--- ppm-react-frontend/src/components/Dashboard.js
+++ ppm-react-frontend/src/components/Dashboard.js
@@ -1,13 +1,27 @@
 import React, { Component, Fragment } from "react";
 import ProjectItem from "./Project/ProjectItem";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CreateProjectButton from "./Project/CreateProjectButton";
 
 class Dashboard extends Component {
   render() {
     return (
-        <Fragment>
-            <h1>Welcome to Dashboard</h1>
-            <ProjectItem />
-        </Fragment>
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+
+              <CreateProjectButton />
+
+              <br />
+              <hr />
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
