c14d5cd81490560b99cd8d5fbeae8c992fc39d41
lequocgiom <letrung95@gmail.com>
2020-01-17 18:36:53 +0700

    adding redux

--- ppm-react-frontend/src/components/Dashboard.js
+++ ppm-react-frontend/src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import ProjectItem from "./Project/ProjectItem";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CreateProjectButton from "./Project/CreateProjectButton";
