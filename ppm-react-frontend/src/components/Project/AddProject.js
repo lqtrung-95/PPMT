a6b603dddc9d49788d74a300159296732211e33b
lequocgiom <letrung95@gmail.com>
2020-01-19 00:07:30 +0700

    Create project from React.

--- ppm-react-frontend/src/components/Project/AddProject.js
+++ ppm-react-frontend/src/components/Project/AddProject.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-
+import PropTypes from "prop-types"; 
+import {connect} from "react-redux";
+import {createProject} from "../../actions/projectActions"
 class AddProject extends Component {
   constructor() {
     super();
@@ -32,7 +34,7 @@ class AddProject extends Component {
       end_date: this.state.end_date
     };
 
-    console.log(newProject)
+    this.props.createProject(newProject, this.props.history);
   }
 
   render() {
@@ -111,4 +113,10 @@ class AddProject extends Component {
   }
 }
 
-export default AddProject;
+AddProject.propTypes = {
+  createProject : PropTypes.func.isRequired
+}
+
+export default connect(
+  null, {createProject}
+  ) (AddProject);
