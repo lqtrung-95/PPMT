2f4ededd716a21b99b30348c4020e438aabb845b
lequocgiom <letrung95@gmail.com>
2020-02-12 22:46:24 +0700

    Find ProjectTask by projectSequence wValidation.

--- PPMT/src/main/java/trungle95/personal/example/PPMT/services/ProjectTaskService.java
+++ PPMT/src/main/java/trungle95/personal/example/PPMT/services/ProjectTaskService.java
@@ -71,8 +71,27 @@ public class  ProjectTaskService {
     }
 
     public ProjectTask findByProjectSequence(String backlog_id, String pt_id) {
+
+        //make sure we are searching on an existing backlog
+        BackLog backLog = backLogRepository.findByProjectIdentifier(backlog_id);
+        if (backLog==null){
+            throw new ProjectNotFoundException("Project with ID: '" +backlog_id+ "' does not exist");
+        }
+
+        //make sure that our task exists
+        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        if(projectTask == null) {
+            throw new ProjectNotFoundException("Project Task '" +pt_id+ "' not found");
+        }
+
+        //make sure the backlog/project id in the path corresponds to the right project
+        if(!projectTask.getProjectIdentifier().equals(backlog_id)){
+            throw new ProjectNotFoundException("Project Task '" +pt_id+ "' does not exist in project: '" +backlog_id);
+        }
+
         //make sure we are searching on the right backlog
 
-        return projectTaskRepository.findByProjectSequence(pt_id);
+        return projectTask;
     }
 }
