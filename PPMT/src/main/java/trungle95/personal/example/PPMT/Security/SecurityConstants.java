9c448331f958a8caee9b537f590e9f5a78546162
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2020-02-18 10:56:40 +0700

    JWT Provider pre-work

---
+++ PPMT/src/main/java/trungle95/personal/example/PPMT/Security/SecurityConstants.java
@@ -0,0 +1,11 @@
+package trungle95.personal.example.PPMT.Security;
+
+public class SecurityConstants {
+
+    public static final String SIGN_UP_URLS = "/api/users/**";
+    public static final String H2_URL = "h2-console/**";
+    public static final String SECRET = "SecretKetToGenJWTs";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 30_000; // 30secs
+}
--- PPMT/src/main/java/trungle95/personal/example/PPMT/repositories/UserRepository.java
+++ PPMT/src/main/java/trungle95/personal/example/PPMT/repositories/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import trungle95.personal.example.PPMT.domain.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+    User getById(Long id);
 }
