3cf98c13cf3b57d64af8c3a6a3be6993d7d5e98d
lqtrung95 <44612846+lequocgiom@users.noreply.github.com>
2020-02-18 18:08:24 +0700

    Doing custom JWT filter.

--- PPMT/src/main/java/trungle95/personal/example/PPMT/Security/SecurityConfig.java
+++ PPMT/src/main/java/trungle95/personal/example/PPMT/Security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import trungle95.personal.example.PPMT.services.CustomUserDetailsService;
 
 import static trungle95.personal.example.PPMT.Security.SecurityConstants.H2_URL;
@@ -31,6 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {return new JwtAuthenticationFilter();}
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -70,5 +74,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(SIGN_UP_URLS).permitAll()
                 .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
